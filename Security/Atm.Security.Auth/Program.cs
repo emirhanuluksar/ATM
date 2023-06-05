c5fa70774ed4194ed123f036b72bfdc27ea135bc
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-06-05 12:13:59 +0300

    All project files added

--- Security/Atm.Security.Auth/Program.cs
+++ Security/Atm.Security.Auth/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -10,8 +11,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
